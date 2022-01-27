97d73b1ad420cf5b2d1f247c665b70a8b3dbf029
Silas Hellinger <silas.hellinger@flachglas.ch>
2022-01-27 14:44:07 +0100

    Feature/tests (#1)
    Add automated connection test

---
+++ .github/workflows/tests.php
@@ -0,0 +1,3 @@
+<?php
+	$dbh = new PDO('informix:host=ifx;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.819;EnableScrollableCursors=1','informix','in4mix');
+

a19015cf252615050592d727bbfc753b5c5dd090
Silas Hellinger <silas.hellinger@flachglas.ch>
2022-01-27 14:32:01 +0100

    try failing test

--- .github/workflows/tests.php
+++ .github/workflows/tests.php
@@ -1,7 +1,3 @@
 <?php
+	$dbh = new PDO('informix:host=ifxs;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.819;EnableScrollableCursors=1','informix','in4mix');
 
-try {
-    $dbh = new PDO('informix:host=ifx;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.utf8;EnableScrollableCursors=1');
-} catch (PDOException $exception) {
-    echo 'No connection possible: ' . $exception;
-}

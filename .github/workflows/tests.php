69602cc0340fa510bd99a3268f3a02e03a2d6a0e
Silas Hellinger <silas.hellinger@flachglas.ch>
2022-03-03 14:57:16 +0100

    Fix db locale mismatch

--- .github/workflows/tests.php
+++ .github/workflows/tests.php
@@ -1,10 +1,11 @@
 <?php
 try {
     $dbh = new PDO(
-        'informix:host=ifx-test;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.1252;EnableScrollableCursors=1',
+        'informix:host=ifx-test;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.819;EnableScrollableCursors=1',
         'informix',
         'in4mix'
     );
+    echo 'database connection successful';
 } catch (PDOException $exception) {
     throw new PDOException($exception->getMessage());
 }

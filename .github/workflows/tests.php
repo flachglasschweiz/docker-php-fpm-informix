7d631670b660b4c37e519f9b359f0763c0a59b27
Silas Hellinger <silas.hellinger@flachglas.ch>
2022-03-03 17:48:04 +0100

    Feature/tests (#2)
    Add an automated database connection test for all images

--- .github/workflows/tests.php
+++ .github/workflows/tests.php
@@ -1,3 +1,12 @@
 <?php
-	$dbh = new PDO('informix:host=ifx;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.819;EnableScrollableCursors=1','informix','in4mix');
 
+try {
+    $dbh = new PDO(
+        'informix:host=ifx-test;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.819;EnableScrollableCursors=1',
+        'informix',
+        'in4mix'
+    );
+    echo 'database connection successful';
+} catch (PDOException $exception) {
+    throw new PDOException($exception->getMessage());
+}

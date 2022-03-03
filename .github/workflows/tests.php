80b7947f88fb05a1202c411b395bee15b8f6946e
Silas Hellinger <silas.hellinger@flachglas.ch>
2022-03-03 14:45:35 +0100

    Fix connection test

--- .github/workflows/tests.php
+++ .github/workflows/tests.php
@@ -1,7 +1,10 @@
 <?php
-
 try {
-    $dbh = new PDO('informix:host=ifx-test;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.1252;EnableScrollableCursors=1');
+    $dbh = new PDO(
+        'informix:host=ifx-test;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_US.1252;EnableScrollableCursors=1',
+        'informix',
+        'in4mix'
+    );
 } catch (PDOException $exception) {
-    echo 'No connection possible: ' . $exception;
+    throw new PDOException($exception->getMessage());
 }
